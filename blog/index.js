5f706744b382dd692f43e2417b22a586e06eb2d1
15915940450 <15915940450@139.com>
2018-05-27 02:37:29 +0800

    rqs, params

--- blog/index.js
+++ blog/index.js
@@ -6,5 +6,8 @@ var app=express();
 app.get('/',function(rqs,rps){
 	rps.send('hello express.');
 });
+app.get('/users/:name/',function(rqs,rps){
+	rps.send('hello, '+rqs.params.name);
+});
 
 app.listen(3300);
