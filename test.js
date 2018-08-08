228b5f85f57097f499b563406c70b16b9c9207fe
15915940450 <15915940450@139.com>
2018-08-08 15:49:58 +0800

    exch in arr

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-var arr=[1,5,9,40];
+var arr=[1,5,9,159,40,450];
 
-console.log(arr[2.5]);
+function exch(a,b,arr){
+  var tmp;
+  tmp=arr[a];
+  arr[a]=arr[b];
+  arr[b]=tmp;
+  return arr;
+}
+
+console.log(exch(1,3,arr));
