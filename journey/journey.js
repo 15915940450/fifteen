29bf0f9a1ebd9a44dab4b684e093e59a2ff5a883
15915940450 <15915940450@139.com>
2018-11-25 20:52:43 -0500

    add arrX, arrY and check

--- journey/journey.js
+++ journey/journey.js
@@ -1,1 +1,14 @@
-console.log('journey.js');
+class Journey{
+  constructor(){
+    this.arrX=[1,2,2,1,-1,-2,-2,-1];  //橫軸
+    this.arrY=[2,1,-1,-2,2,1,-1,-2];  //豎軸
+  }
+
+  check(){
+    var pass=true;
+    return pass;
+  }
+} //class
+
+var obj=new Journey();
+obj.check();
