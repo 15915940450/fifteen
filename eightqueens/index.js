aa356b72974ddc3d801c031285aee60237d84bfb
15915940450 <15915940450@139.com>
2018-12-18 04:37:26 -0500

    result is init as empty array

--- eightqueens/index.js
+++ eightqueens/index.js
@@ -14,25 +14,30 @@
 // queen([1,1,1,1,1,1,1,1],0);
 class EightQueens{
   constructor(){
-    this.result=[-1,-1,-1,-1,-1,-1,-1,-1];
+    this.result=[];
     this.okay=false;
   }
 
+  //解決方案
   solve(){
     var es6This=this;
     es6This.okay=es6This.tryrow(0);
     console.log(es6This.okay);
     return es6This;
   }
+  //一行一行來試
   tryrow(row){
     var es6This=this;
     if(row>=8){
       return true;
     }
-
+    //每一行裏的操作：一列一列的試
     for(var col=0;col<8;col++){
-      var isPassCurrent=es6This.check(row,col);
+      //八皇后的解
+      this.result[row]=col;
+      var isPassCurrent=es6This.check(row);
       if(!isPassCurrent){
+        //下一列
         continue;
       }
 
@@ -40,7 +45,9 @@ class EightQueens{
       //下一行
       var isPassNext=es6This.tryrow(row+1);
       if(!isPassNext){
-        //回溯
+        es6This.result.length=row;
+        console.log('回溯:',es6This.result);
+        //回溯,下一列
         continue;
       }
       return true;
@@ -50,17 +57,49 @@ class EightQueens{
 
     return false;
   }
-  check(){
+  //檢查this.result[row]下是否可行，沒有被攻擊
+  check(row){
     var es6This=this;
-  
+    
+    // console.log(row);
+    //1.列
+    var checkCol=es6This.result.findIndex(function(v){
+      return (v===es6This.result[row]);
+    });
+    // console.log(checkCol);
+    if(checkCol!==row){
+      return false;
+    }
+    //2.45deg(有一些)
+    var check45=es6This.result.some(function(v,i){
+      // console.log(i,v);
+      if(v!==-1 && i!==row){
+        return (row-i===es6This.result[row]-v);
+      }
+    });
+    if(check45){
+      return false;
+    }
+
     return true;
   }
+  result2cell(){
+    var es6This=this;
+    var arrCellIndex=es6This.result.map(function(v,i){
+      return (i*8+v);
+    });
+    return arrCellIndex;
+  }
+  //DOM
   html(){
     var es6This=this;
+    var arrCellIndex=es6This.result2cell();
     var arr=[];
     arr.length=64;
     var strHTML=arr.fill(0).map(function(v,i){
-      return (`<div class="${i&1^i>>>3&1?'black':''}"></div>`);
+      var red=arrCellIndex.includes(i)?'red':''
+      //i&1^i>>>3&1  偶數行奇數列或奇數行偶數列
+      return (`<div class="${i&1^i>>>3&1?'black':''} ${red}"></div>`);
     }).join('');
     document.getElementById('wrap').innerHTML=strHTML;
     return es6This;
@@ -68,4 +107,5 @@ class EightQueens{
 }
 
 var obj=new EightQueens();
-obj.solve();
+obj.solve().html();
+console.log(obj.result);
