b3e9239c31621a9d5173f9330ab29e1fde1aa5c4
15915940450 <15915940450@139.com>
2018-12-25 00:47:18 -0500

    wiki pedia

--- o1knapsack/index.js
+++ o1knapsack/index.js
@@ -3,37 +3,49 @@
 0-1 knapsack problem
 
 動態規劃狀態轉移方程->
-f(0,w)=0;
-f(i,w)=f(i-1,w)  if wi>w
-f(i,w)=max{ f(i-1,w), f(i-1,w-wi)+vi }   if wi<=w
+f(0,w)=0;                                                   (part0)
+f(i,w)=f(i-1,w)  if wi>w                                    (part1)
+f(i,w)=max{ f(i-1,w), f(i-1,w-wi)+vi }   if wi<=w           (part2)
+
+
+i\w   0 1 2 3 4 5 6  7  8  9  10
+    ------------------------------
+0   | [0,0,0,0,0,0,0 ,0, 0, 0, 0 ],
+id1 | [0,0,6,6,6,6,6 ,6 ,6 ,6 ,6 ],
+id2 | [0,0,6,6,6,6,6 ,10,10,10,10],
+id3 | [0,0,6,6,6,6,6 ,10,11,11,11],
+id4 | [0,0,6,6,9,9,9 ,10,11,13,14],
+id5 | [0,0,6,6,9,9,12,12,15,15,15]
+
 */
+// if choose->>> matrix[i][j]=es6This.matrix[i-1][j-es6This.n[i-1].weight]+es6This.n[i-1].values;
 
 class O1knapsack{
   constructor(){
     this.W=10;  //能够携重W的背包
     this.n=[
       {
-        id:0,
+        id:1,
         weight:2,
         values:6
       },
       {
-        id:3,
+        id:2,
         weight:5,
         values:4
       },
       {
-        id:2,
+        id:3,
         weight:6,
         values:5
       },
       {
-        id:1,
+        id:4,
         weight:2,
         values:3
       },
       {
-        id:4,
+        id:5,
         weight:4,
         values:6
       }
@@ -48,29 +60,21 @@ class O1knapsack{
   //解決方案
   solve(){
     var es6This=this;
-    
-
-    //第一行：邊界i===0
-    for(var j=0;j<=es6This.W;j++){
-      es6This.matrix[0]=es6This.matrix[0] || [];
-      if(j<es6This.n[0].weight){
-        es6This.matrix[0][j]=0;
-      }else{
-        es6This.matrix[0][j]=this.n[0].values;
-      }
-    }
 
     //動態規劃遞推
-    for(j=0;j<=es6This.W;j++){
-      for(var i=1;i<es6This.nLength;i++){
+    for(var j=0;j<=es6This.W;j++){
+      for(var i=0;i<=es6This.nLength;i++){
         es6This.matrix[i]=es6This.matrix[i] || [];
-        if(j<es6This.n[i].weight){
+        if(i===0){
+          //公式part0
+          es6This.matrix[i][j]=0;
+        }else if(es6This.n[i-1].weight>j){
           //公式part1
           es6This.matrix[i][j]=es6This.matrix[i-1][j];
         }else{
           //公式part2
           var vNoChooseI=es6This.matrix[i-1][j];
-          var vChooseI=es6This.matrix[i-1][j-es6This.n[i].weight]+es6This.n[i].values;
+          var vChooseI=es6This.matrix[i-1][j-es6This.n[i-1].weight]+es6This.n[i-1].values;
           
           es6This.matrix[i][j]=Math.max(vNoChooseI,vChooseI);
         }
@@ -78,27 +82,24 @@ class O1knapsack{
     }
 
     //最右下角的值即爲最大價值
-    es6This.maxValues=es6This.matrix[es6This.nLength-1][es6This.W]; //得出最大值
+    es6This.maxValues=es6This.matrix[es6This.nLength][es6This.W]; //得出最大值
 
     //查找解的物品構成
-    es6This.findN(es6This.nLength-1,es6This.W);
+    es6This.findN(es6This.nLength,es6This.W);
     return es6This;
   }
   findN(i,j){
     var es6This=this;
+    //遞歸出口
     if(i<=0){
-      if(es6This.matrix[0][j]){
-        //第0個物品是否可選
-        es6This.result.push(es6This.n[0]);
-      }
       return false;
     }
 
     if(es6This.matrix[i][j]===es6This.matrix[i-1][j]){
       es6This.findN(i-1,j);
     }else{
-      es6This.result.push(es6This.n[i]);
-      es6This.findN(i-1,j-es6This.n[i].weight);
+      es6This.result.push(es6This.n[i-1]);
+      es6This.findN(i-1,j-es6This.n[i-1].weight);
     }
 
   }
@@ -107,3 +108,4 @@ class O1knapsack{
 var obj=new O1knapsack();
 obj.solve();
 console.log(obj.result,obj.maxValues);  //[obj.nLength-1][obj.W]
+console.log(JSON.stringify(obj.matrix));
