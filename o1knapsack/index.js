68fe391ed188995e20253967bd6ef872f4e035ef
15915940450 <15915940450@139.com>
2018-12-25 00:08:29 -0500

    find N

--- o1knapsack/index.js
+++ o1knapsack/index.js
@@ -8,9 +8,9 @@ class O1knapsack{
         values:6
       },
       {
-        id:1,
-        weight:2,
-        values:3
+        id:3,
+        weight:5,
+        values:4
       },
       {
         id:2,
@@ -18,9 +18,9 @@ class O1knapsack{
         values:5
       },
       {
-        id:3,
-        weight:5,
-        values:4
+        id:1,
+        weight:2,
+        values:3
       },
       {
         id:4,
@@ -30,14 +30,15 @@ class O1knapsack{
     ];
     this.nLength=this.n.length;
 
-    this.matrix=[]; //nLength*W
-
-    this.result=[];
+    this.matrix=[]; //矩陣表：二維數組
+    this.maxValues=0; //最大價值
+    this.result=[]; //構成最大價值的那些物品
   }
 
+  //解決方案
   solve(){
     var es6This=this;
-    this.initMatrix();
+    
 
     //第一行：邊界i===0
     for(var j=0;j<=es6This.W;j++){
@@ -49,27 +50,50 @@ class O1knapsack{
       }
     }
 
+    //動態規劃遞推
     for(j=0;j<=es6This.W;j++){
       for(var i=1;i<es6This.nLength;i++){
         es6This.matrix[i]=es6This.matrix[i] || [];
         if(j<es6This.n[i].weight){
+          //公式part1
           es6This.matrix[i][j]=es6This.matrix[i-1][j];
         }else{
+          //公式part2
           var vNoChooseI=es6This.matrix[i-1][j];
           var vChooseI=es6This.matrix[i-1][j-es6This.n[i].weight]+es6This.n[i].values;
+          
           es6This.matrix[i][j]=Math.max(vNoChooseI,vChooseI);
         }
       }
     }
 
+    //最右下角的值即爲最大價值
+    es6This.maxValues=es6This.matrix[es6This.nLength-1][es6This.W]; //得出最大值
+
+    //查找解的物品構成
+    es6This.findN(es6This.nLength-1,es6This.W);
     return es6This;
   }
-  initMatrix(){
+  findN(i,j){
     var es6This=this;
-    return es6This;
+    if(i<=0){
+      if(es6This.matrix[0][j]){
+        //第0個物品是否可選
+        es6This.result.push(es6This.n[0]);
+      }
+      return false;
+    }
+
+    if(es6This.matrix[i][j]===es6This.matrix[i-1][j]){
+      es6This.findN(i-1,j);
+    }else{
+      es6This.result.push(es6This.n[i]);
+      es6This.findN(i-1,j-es6This.n[i].weight);
+    }
+
   }
 }
 
 var obj=new O1knapsack();
 obj.solve();
-console.log(obj.matrix[obj.nLength-1][obj.W]);
+console.log(obj.result,obj.maxValues);  //[obj.nLength-1][obj.W]
