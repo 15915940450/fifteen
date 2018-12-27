76b89e80522bd0b81f2929db40d223fb22cd5a9a
15915940450 <15915940450@139.com>
2018-12-27 01:55:03 -0500

    timer to search best

--- travelling_salesman_problem/index.js
+++ travelling_salesman_problem/index.js
@@ -5,6 +5,11 @@ class Travelling{
     this.points=[{'id':0,'x':253,'y':148},{'id':1,'x':644,'y':222},{'id':2,'x':694,'y':204},{'id':3,'x':368,'y':192},{'id':4,'x':178,'y':170},{'id':5,'x':492,'y':144},{'id':6,'x':136,'y':247},{'id':7,'x':491,'y':227},{'id':8,'x':589,'y':269},{'id':9,'x':194,'y':144},{'id':10,'x':501,'y':43},{'id':11,'x':43,'y':247},{'id':12,'x':474,'y':39},{'id':13,'x':635,'y':115},{'id':14,'x':192,'y':198},{'id':15,'x':305,'y':224},{'id':16,'x':720,'y':38},{'id':17,'x':616,'y':78},{'id':18,'x':391,'y':153},{'id':19,'x':47,'y':219},{'id':20,'x':584,'y':89},{'id':21,'x':769,'y':149},{'id':22,'x':45,'y':166},{'id':23,'x':334,'y':142},{'id':24,'x':313,'y':46},{'id':25,'x':246,'y':176},{'id':26,'x':517,'y':118},{'id':27,'x':637,'y':178},{'id':28,'x':161,'y':176},{'id':29,'x':371,'y':39},{'id':30,'x':147,'y':56},{'id':31,'x':117,'y':113},{'id':32,'x':229,'y':186},{'id':33,'x':641,'y':127},{'id':34,'x':337,'y':26},{'id':35,'x':242,'y':52},{'id':36,'x':356,'y':231},{'id':37,'x':224,'y':216},{'id':38,'x':138,'y':146},{'id':39,'x':245,'y':256},{'id':40,'x':625,'y':237},{'id':41,'x':118,'y':41},{'id':42,'x':741,'y':193},{'id':43,'x':584,'y':114},{'id':44,'x':371,'y':216},{'id':45,'x':514,'y':182},{'id':46,'x':25,'y':90},{'id':47,'x':661,'y':85},{'id':48,'x':575,'y':44},{'id':49,'x':601,'y':227}];
 
     this.ctx=null;
+    this.ctxAfter=null;
+    this.best={
+      distance:0,
+      points:[]
+    };
   }
 
   initPoints(){
@@ -21,34 +26,34 @@ class Travelling{
     return es6This;
   }
 
-  draw(){
+  draw(id){
     var es6This=this;
-    var canvas=document.getElementById('canvas');
-    es6This.ctx=canvas.getContext('2d');
-
-    // es6This.points.sort(function(){
-    //   return (Math.random()-0.5);
-    // });
-    //畫點
-
-    es6This.drawWithCTX(es6This.ctx);
+    id=id || 'canvas';
+    var canvas=document.getElementById(id);
+    if(id==='canvasAfter'){
+      es6This.ctxAfter=canvas.getContext('2d');
+    }else{
+      es6This.ctx=canvas.getContext('2d');
+    }
     return es6This;
   }
-  drawWithCTX(ctx){
+  drawWithCTX(ctx,points){
     var es6This=this;
+    ctx=ctx || es6This.ctx;
+    points=points || es6This.points;
     ctx.clearRect(0,0,es6This.canvasWidth,es6This.canvasHeight);
-    for(var i=0;i<es6This.points.length;i++){
+    for(var i=0;i<points.length;i++){
       ctx.beginPath();
-      ctx.arc(es6This.points[i].x,es6This.points[i].y,4,0,Math.PI*2,true);
+      ctx.arc(points[i].x,points[i].y,4,0,Math.PI*2,true);
       ctx.fill();
     }
 
     //畫曲綫
     ctx.beginPath();
-    ctx.moveTo(es6This.points[0].x,es6This.points[0].y);
+    ctx.moveTo(points[0].x,points[0].y);
 
-    for(i=1;i<es6This.points.length;i++){
-      ctx.lineTo(es6This.points[i].x,es6This.points[i].y);
+    for(i=1;i<points.length;i++){
+      ctx.lineTo(points[i].x,points[i].y);
     }
 
     ctx.closePath();
@@ -56,19 +61,55 @@ class Travelling{
     return es6This;
   }
 
+  //定時器
   timer(){
     var es6This=this;
     window.setInterval(function(){
       es6This.points.sort(function(){
         return (Math.random()-0.5);
       });
-      es6This.drawWithCTX(es6This.ctx);
+      es6This.drawWithCTX();
+      es6This.calcDistance();
     },1);
+    return es6This;
+  }
+  //計算所有點長度
+  calcDistance(){
+    var es6This=this;
+    var distance=es6This.points.reduce(function(acc,cur,idx,src){
+      if(idx){
+        //1,2,3,4...
+        var powX=Math.pow(src[idx].x-src[idx-1].x,2);
+        var powY=Math.pow(src[idx].y-src[idx-1].y,2);
+        var distance2idx=Math.sqrt(powX+powY);
+        return (acc+distance2idx);
+      }else{
+        return (0);
+      }
+    },0)>>0;
+    //set best
+    if(!es6This.best.distance){
+      es6This.best.distance=distance;
+    }
+    if(distance<es6This.best.distance){
+      es6This.best={
+        distance:distance,
+        points:es6This.points.slice()
+      };
+      es6This.drawBest();
+    }
+    return es6This;
+  }
+
+  drawBest(){
+    var es6This=this;
+    es6This.draw('canvasAfter').drawWithCTX(es6This.ctxAfter,es6This.best.points);
+    console.log(es6This.best.distance);
     return es6This;
   }
 
 }  //class
 
 var obj=new Travelling();
-obj.draw().timer();
+obj.draw().drawWithCTX().calcDistance().timer();
 
