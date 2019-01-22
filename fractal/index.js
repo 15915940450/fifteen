6d697b689f51b5847b4439bc60cd33b028c3b22f
15915940450 <15915940450@139.com>
2019-01-22 16:41:41 +0800

    basic draw

--- fractal/index.js
+++ fractal/index.js
@@ -3,15 +3,33 @@ class Fractal{
     this.eleFractal=document.querySelector('#fractal');
     this.CW=document.documentElement.clientWidth || document.body.clientWidth;
     this.CH=document.documentElement.clientHeight || document.body.clientHeight;
+
+    this.ctx=this.eleFractal.getContext('2d');
   }
 
   init(){
     var es6This=this;
+    //設置canvas的寬高
     es6This.eleFractal.width=es6This.CW;
-    es6This.eleFractal.height=es6This.CH-4;
+    es6This.eleFractal.height=es6This.CH-4; //高減4，避免出現滾動條
+    return es6This;
+  }
+
+  draw(){
+    var es6This=this;
+    var ctx=es6This.ctx;
+    //把原點移到中綫下方位置
+    ctx.translate(es6This.CW/2,es6This.CH-4);
+    ctx.strokeStyle='blanchedalmond';
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,-200);
+    ctx.lineTo(200,-200);
+    // ctx.closePath();
+    ctx.stroke();
     return es6This;
   }
 } //class
 
 var obj=new Fractal();
-obj.init();
+obj.init().draw();
