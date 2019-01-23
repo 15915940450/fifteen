5650a4041b0d97a4cbdd47393122908db06f2646
15915940450 <15915940450@139.com>
2019-01-23 10:53:27 +0800

    canvas state

--- fractal/index.js
+++ fractal/index.js
@@ -1,3 +1,8 @@
+/*
+* js注釋：
+--分形樹
+*/
+
 class Fractal{
   constructor(){
     this.eleFractal=document.querySelector('#fractal');
@@ -5,6 +10,11 @@ class Fractal{
     this.CH=document.documentElement.clientHeight || document.body.clientHeight;
 
     this.ctx=this.eleFractal.getContext('2d');
+    this.rootLen=220;
+    this.rate=0.77;
+    this.rad=Math.PI/15;
+
+    this.count=0;
   }
 
   init(){
@@ -17,19 +27,43 @@ class Fractal{
 
   draw(){
     var es6This=this;
+    
     var ctx=es6This.ctx;
     //把原點移到中綫下方位置
     ctx.translate(es6This.CW/2,es6This.CH-4);
     ctx.strokeStyle='blanchedalmond';
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(0,-200);
-    ctx.lineTo(200,-200);
+
+    //遞歸調用繪製分支
+    es6This.drawBranch(es6This.rootLen);
+    
     // ctx.closePath();
     ctx.stroke();
+    return es6This;
+  }
+  drawBranch(gth){
+    var es6This=this;
+    // 分支長度小於 _ 則終止繪製
+    if(gth<4){
+      return (false);
+    }
+    es6This.count++;
+
+    var ctx=es6This.ctx;
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,-gth); //1
+    // ctx.stroke();
+    ctx.translate(0,-gth);
+    ctx.save();
+    ctx.rotate(es6This.rad);
+    es6This.drawBranch(gth*es6This.rate);  //2:畫右分支
+    ctx.restore();
+    ctx.rotate(-es6This.rad);
+    es6This.drawBranch(gth*es6This.rate); //3:畫左分支
     return es6This;
   }
 } //class
 
 var obj=new Fractal();
 obj.init().draw();
+console.log(obj.count);
