fc115a8ee0c10af0bf89eb0e308a5db7e31975d0
15915940450 <15915940450@139.com>
2019-01-26 13:10:45 +0800

    y is sin

--- fourierseries/index.js
+++ fourierseries/index.js
@@ -8,11 +8,13 @@ class Fourier{
     this.ctx=this.eleFourier.getContext('2d');
 
     this.radius=0;  //圓半徑
+    this.smallRadius=4;
     this.radSpeed=0.02;
     this.largestRad=1e6;
     this.originXY=300;
-    this.maxLength=1e2;
-    this.percent='0.000000000000000000000000000000000000000%';
+    this.maxLength=1e3;
+    this.percent='0.0000%';
+    this.n=1;
 
     this.arrSineWavePoint=[]; //真正的正弦波點
     this.arrSineWavePointOrigin=[];
@@ -63,28 +65,39 @@ class Fourier{
 
     
 
-    
+    var i;
     var x=0;
     var y=0;
     var xSmall=0;
     var ySmall=0;
 
-    for(var i=0;i<2;i++){
-      es6This.radius=100*4/(Math.PI*(i*2+1));
+    for(i=0;i<es6This.n;i++){
+      var n=i*2+1;
+      es6This.radius=100*4/(Math.PI*n);
+
       //畫圓(圓心0，0)
       ctx.beginPath();
-      ctx.arc(x,y,es6This.radius,0,Math.PI*2);
+      ctx.arc(xSmall,ySmall,es6This.radius,0,Math.PI*2);
       // ctx.closePath();
       ctx.stroke();
 
       //畫小圓點
       ctx.beginPath();
-      x=es6This.radius*Math.sin((i*2+1)*es6This.rad);
-      y=es6This.radius*Math.cos((i*2+1)*es6This.rad);
+      x=es6This.radius*Math.cos(es6This.rad*n);
+      y=es6This.radius*Math.sin(es6This.rad*n);
       xSmall+=x;
       ySmall+=y;
-      ctx.arc(xSmall,ySmall,5,0,Math.PI*2);
-      ctx.fill();
+      
+      if(i===es6This.n-1){
+        //最後一個點
+        ctx.fillStyle='crimson';
+        ctx.arc(xSmall,ySmall,es6This.smallRadius,0,Math.PI*2);
+        ctx.fill();
+        ctx.fillStyle='antiquewhite';
+      }else{
+        ctx.arc(xSmall,ySmall,es6This.smallRadius,0,Math.PI*2);
+        ctx.fill();
+      }
 
       //畫圓心到小圓點的連綫
       ctx.beginPath();
@@ -101,8 +114,9 @@ class Fourier{
 
     //arrSineWavePointOrigin==>arrSineWavePoint
     es6This.arrSineWavePointOrigin.unshift({
-      x:-es6This.rad*es6This.radius/2,
-      y:y
+      // x:-es6This.rad*es6This.radius/2,
+      x:-100*es6This.rad,
+      y:ySmall
     });
     if(es6This.arrSineWavePointOrigin.length>es6This.maxLength){
       es6This.arrSineWavePointOrigin.length=es6This.maxLength;
@@ -111,7 +125,7 @@ class Fourier{
     var gap=es6This.arrSineWavePointOrigin[0].x;
     es6This.arrSineWavePoint=es6This.arrSineWavePointOrigin.map(function(v){
       return ({
-        x:v.x-gap+es6This.radius*2,
+        x:v.x-gap+300,
         y:v.y
       });
     });
@@ -119,7 +133,7 @@ class Fourier{
     //畫小圓點映射到正弦波的連綫
     ctx.strokeStyle='darkviolet';
     ctx.beginPath();
-    ctx.moveTo(x,y);
+    ctx.moveTo(xSmall,ySmall);
     ctx.lineTo(es6This.arrSineWavePoint[0].x,es6This.arrSineWavePoint[0].y);
     ctx.stroke();
     ctx.strokeStyle='antiquewhite';
@@ -147,7 +161,7 @@ class Fourier{
     //畫sine wave
     ctx.beginPath();
     ctx.moveTo(es6This.arrSineWavePoint[0].x,es6This.arrSineWavePoint[0].y);
-    for(var i=1;i<es6This.arrSineWavePoint.length;i++){
+    for(i=1;i<es6This.arrSineWavePoint.length;i++){
       ctx.lineTo(es6This.arrSineWavePoint[i].x,es6This.arrSineWavePoint[i].y);
     }
     ctx.stroke();
