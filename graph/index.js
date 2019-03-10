eda07d5c17cf59fa262500d6bbcd6a7238b41ac6
15915940450 <15915940450@139.com>
2019-03-10 08:46:15 +0800

    init just see the constructor

--- graph/index.js
+++ graph/index.js
@@ -1,46 +1,23 @@
 class Graph{
   constructor(){
     this.queue=[];  //隊列
-    this.marked=[]; //標志是否已經經過
-    this.edgeTo=[]; //軌跡
+    this.stack=[];  //堆
+    this.marked=[]; //標志是否已經訪問過
+    this.edgeTo=[]; //軌跡，由哪個頂點過來的
     this.adj=[]; //鄰接表
 
-    this.depth=0;
-
-    this.gthV=6;  //頂點個數
+    this.gthV=0;  //頂點個數
 
     this.pathDFS=[];  //由深度優先搜索所得路徑
-    
     this.pathBFS=[];  //由廣度優先搜索所得路徑
   }
 
   //初始化
   init(){
-    var es6This=this;
-    es6This.initAdj();
-    es6This.initMarked();
-    // console.log(es6This.adj);
-    // es6This.findPathByDFS(0,4);
-    // es6This.initQueue();
-    es6This.findPathByBFS(0,4);
-    return es6This;
-  }
-  initQueue(){
-    var es6This=this;
-    return es6This;
-  }
-  //初始化各個節點的訪問情況
-  initMarked(){
-    var es6This=this;
-    for(var i=0;i<es6This.gthV;i++){
-      es6This.marked[i]=false;
-    }
-    return es6This;
-  }
-  //初始化圖
-  initAdj(){
-    var es6This=this;
-    es6This.adj=[
+    var f=this;
+    var i;
+    //初始化圖
+    f.adj=[
       [1,2,5],  //0
       [0,2],    //1
       [0,1,3,4],//2
@@ -48,7 +25,14 @@ class Graph{
       [2,3],    //4
       [0,3]     //5
     ];
-    return es6This;
+    //初始化頂點個數
+    f.gthV=f.adj.length;
+    //初始化各個節點的訪問情況
+    for(i=0;i<f.gthV;i++){
+      f.marked[i]=false;
+    }
+    f.findPathByBFS(0,4);
+    return f;
   }
   //深度優先搜索
   findPathByDFS(v,w){
@@ -66,36 +50,38 @@ class Graph{
       break;
     }
     return false;
-
   }
   //廣度優先搜索
   findPathByBFS(v,w){
-    var es6This=this;
+    var f=this;
+    var i;
     if(w===undefined){
       w=v;
     }else{
-      es6This.queue.push(v);
+      //初始化隊列
+      f.queue.push(v);
+      f.edgeTo[v]=-1;
     }
     //終止條件
-    if(es6This.queue.includes(w)){
-      console.log(es6This.queue);
-      console.log(es6This.edgeTo);
+    //if(!f.queue.length){
+    if(f.queue.includes(w)){
+      console.log(f.edgeTo);
       return true;
     }
 
-    // 循環
-    var currentV=es6This.queue.shift();
-    es6This.marked[currentV]=true;
-    for(var i=0;i<es6This.adj[currentV].length;i++){
-      var vertex=es6This.adj[currentV][i];
-      if(!es6This.marked[vertex]){
-        es6This.queue.push(vertex);
-        es6This.marked[vertex]=true;
-        es6This.edgeTo[vertex]=currentV;
+    // 重複步驟
+    var currentV=f.queue.shift();
+    f.marked[currentV]=true;
+    for(i=0;i<f.adj[currentV].length;i++){
+      var vertex=f.adj[currentV][i];
+      if(!f.marked[vertex]){
+        f.queue.push(vertex);
+        f.edgeTo[vertex]=currentV;
+        f.marked[vertex]=true;
       }
     }
 
-    es6This.findPathByBFS(w);
+    f.findPathByBFS(w);
     return false;
   }
 } //class
