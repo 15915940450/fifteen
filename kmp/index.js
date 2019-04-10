f8500fd75a2d01ad4941eb5188c6075d0b720018
15915940450 <15915940450@139.com>
2019-04-10 15:25:24 +0800

    start kmp

---
+++ kmp/index.js
@@ -0,0 +1,53 @@
+// https://www.bilibili.com/video/av11866460?from=search&seid=18054295925786167084
+class KMP{
+  constructor(){
+    this.n=-1;  //raf多少次
+    this.interval=10; //每幀的間隔
+    this.currentStep=-1; //當前。。。
+
+    this.S='KMP算法是一种改进的字符串匹配算法，由D.E.Knuth，J.H.Morris和V.R.Pratt同时发现，因此人们称它为克努特——莫里斯——普拉特操作（简称KMP算法）。KMP算法的关键是利用匹配失败后的信息，尽量减少模式串与主串的匹配次数以达到快速匹配的目的。具体实现就是实现一个next()函数，函数本身包含了模式串的局部匹配信息。时间复杂度O(m+n)。';
+    this.P='字符串匹配算法';
+  }
+
+  init(){
+    document.querySelector('#s').innerHTML=this.S;
+    document.querySelector('#p').innerHTML=this.P;
+  }
+
+  // 算法
+  solve(){
+    var f=this;
+    f.raf();
+    return f;
+  }
+  //執行動畫
+  raf(){
+    var f=this;
+    var rafCallback=function(){
+      f.n++;
+      //動畫終止條件
+      if(f.n<1e1*f.interval){
+        if(!(f.n%f.interval)){
+          //若n加了10, currentStep加了1
+          f.currentStep=f.n/f.interval;
+          f.doINeveryframe();
+        }
+        window.requestAnimationFrame(rafCallback);
+      }
+    };
+    window.requestAnimationFrame(rafCallback);
+    return f;
+  } //raf
+  //每一幀你要做點什麽？
+  doINeveryframe(){
+    var f=this;
+    console.log(f.currentStep);
+    console.log(f.S.match(f.P));
+    return f;
+  }
+
+} //class
+
+var obj=new KMP();
+obj.init();
+obj.solve();
