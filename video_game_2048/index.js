cbf502d4c928349d91f2ca19e1dd73a02f8e5fea
15915940450 <15915940450@139.com>
2019-04-17 18:23:13 +0800

    handle 39

--- video_game_2048/index.js
+++ video_game_2048/index.js
@@ -1,7 +1,18 @@
-new Vue({
+var obj=new Vue({
   el:'#container',
   data:{
-    items:[],
+    items:(function(){
+      var i,arrItems=[];
+      for(i=0;i<16;i++){
+        arrItems[i]={
+          index:i,
+          i:i/4>>0,  //行
+          j:i%4,  //列
+          value:0
+        };
+      }
+      return arrItems;
+    })(),
     initN:5
   },
   methods:{
@@ -9,28 +20,67 @@ new Vue({
       this.items=_.shuffle(this.items);
     },
     init:function(){
-      var i;
-      for(i=0;i<16;i++){
-        this.items[i]={
-          index:i,
-          i:0,  //行
-          j:0,  //列
-          value:0
-        };
-      }
-
-      for(i=0;i<this.initN;i++){
+      for(var i=0;i<this.initN;i++){
         this.addNumber();
       }
     },  //init
+
+    //產生新數字
     addNumber:function(){
       var items_value0=this.items.filter(function(v){
         return !(+v.value);
       });
 
-      var index=this.pickRandom(items_value0).index;
-      this.items[index].value=Math.random()>.2?2:4;
+      var randomOne=this.pickRandom(items_value0);
+      if(randomOne){
+        var index=randomOne.index;
+        this.items=this.items.map(function(v){
+          if(v.index===index){
+            v.value=Math.random()>.2?2:4;
+          }
+          return (v);
+        });
+      }else{
+        console.log('沒有空餘的tile');
+      }
+      
     },
+    index2ij:function(index){
+      return ({
+        i:index/4>>0,
+        j:index%4
+      });
+    },
+    //滑動(right)
+    slide:function(arr){
+      var arrResult=arr.filter(function(v){
+        return (+v.value);
+      });
+      var arrMissing=arr.filter(function(v){
+        return (!+v.value);
+      });
+      arrResult=arrMissing.concat(arrResult);
+      return arrResult;
+    },
+    to2Drow:function(arr){
+      return (_.chunk(arr,4));
+    },
+    handleKey(keyCode){
+      var f=this;
+      switch(+keyCode){
+      case 39:
+        this.items=_.flatten(this.to2Drow(this.items).map(function(v){
+          return (f.slide(v));
+        }));
+        this.addNumber();
+        break;
+      case -1:
+        break;
+      default:
+        console.log(keyCode);
+      }
+    },
+    //從數組中隨機選出一個元素
     pickRandom:function(arr){
       return (arr[arr.length*Math.random()>>0]);
     }
@@ -39,3 +89,8 @@ new Vue({
     this.init();
   }
 });
+
+
+document.onkeydown=function(ev){
+  obj.handleKey(ev.keyCode);
+};
