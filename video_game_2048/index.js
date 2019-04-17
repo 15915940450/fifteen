47e8237b3e5175495f591cc92fdc3176cb244bfa
15915940450 <15915940450@139.com>
2019-04-17 16:36:22 +0800

    pick random

--- video_game_2048/index.js
+++ video_game_2048/index.js
@@ -1,12 +1,41 @@
 new Vue({
   el:'#container',
   data:{
-    items:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    items:[],
+    initN:5
   },
   methods:{
     shuffle:function(){
       this.items=_.shuffle(this.items);
-      // console.table(this.items);
+    },
+    init:function(){
+      var i;
+      for(i=0;i<16;i++){
+        this.items[i]={
+          index:i,
+          i:0,  //行
+          j:0,  //列
+          value:0
+        };
+      }
+
+      for(i=0;i<this.initN;i++){
+        this.addNumber();
+      }
+    },  //init
+    addNumber:function(){
+      var items_value0=this.items.filter(function(v){
+        return !(+v.value);
+      });
+
+      var index=this.pickRandom(items_value0).index;
+      this.items[index].value=Math.random()>.2?2:4;
+    },
+    pickRandom:function(arr){
+      return (arr[arr.length*Math.random()>>0]);
     }
+  },
+  created:function(){
+    this.init();
   }
 });
