4f988cf60491a76501a7c4ad8b53be7cc1a008df
15915940450 <15915940450@139.com>
2019-04-18 11:18:10 +0800

    slide 39

--- video_game_2048/index.js
+++ video_game_2048/index.js
@@ -8,6 +8,7 @@ var obj=new Vue({
           index:i,
           i:i/4>>0,  //行
           j:i%4,  //列
+          isNew:false,
           value:0
         };
       }
@@ -16,17 +17,19 @@ var obj=new Vue({
     initN:5
   },
   methods:{
+    //洗牌
     shuffle:function(){
       this.items=_.shuffle(this.items);
     },
+    //初始化：添加initN個瓷磚（標志不是新瓷磚）
     init:function(){
       for(var i=0;i<this.initN;i++){
-        this.addNumber();
+        this.addNumber(true);
       }
     },  //init
 
     //產生新數字
-    addNumber:function(){
+    addNumber:function(notMarkNew){
       var items_value0=this.items.filter(function(v){
         return !(+v.value);
       });
@@ -35,8 +38,13 @@ var obj=new Vue({
       if(randomOne){
         var index=randomOne.index;
         this.items=this.items.map(function(v){
+          v.isNew=false;
           if(v.index===index){
             v.value=Math.random()>.2?2:4;
+            if(!notMarkNew){
+              //是新瓷磚（參數為false或不傳入）
+              v.isNew=true;
+            }
           }
           return (v);
         });
@@ -45,11 +53,23 @@ var obj=new Vue({
       }
       
     },
+    //取消標志新瓷磚
+    cancelNew(){
+      this.items=this.items.map(function(v){
+        v.isNew=false;
+        return (v);
+      });
+    },
+    //索引推出行列（i,j）
     index2ij:function(index){
       return ({
         i:index/4>>0,
         j:index%4
       });
+    },
+    //行列推出索引
+    ij2index(i,j){
+      return (i*4+j);
     },
     //滑動(right)
     slide:function(arr){
@@ -62,22 +82,34 @@ var obj=new Vue({
       arrResult=arrMissing.concat(arrResult);
       return arrResult;
     },
+    //轉換為二維數組
     to2Drow:function(arr){
       return (_.chunk(arr,4));
     },
+    //監聽上下左右鍵(38,39,40,37)
     handleKey(keyCode){
       var f=this;
       switch(+keyCode){
       case 39:
-        this.items=_.flatten(this.to2Drow(this.items).map(function(v){
-          return (f.slide(v));
-        }));
-        this.addNumber();
+        f.cancelNew();
+        //1.轉換為二位數組
+        var x=f.to2Drow(f.items);
+        //2.每一行（4個元素）右滑
+        x=x.map(function(row){
+          return (f.slide(row));
+        });
+        //設置items
+        f.items=_.flatten(x);
+
+        //半秒之後新增一個數字
+        window.setTimeout(function(){
+          f.addNumber();
+        },3e2);
         break;
       case -1:
         break;
       default:
-        console.log(keyCode);
+        // console.log(keyCode);
       }
     },
     //從數組中隨機選出一個元素
