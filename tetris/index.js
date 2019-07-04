9f5fea15da6ed38b13ce6073d917e849e21882aa
15915940450 <15915940450@139.com>
2019-07-04 10:15:03 +0800

    render canvas

--- tetris/index.js
+++ tetris/index.js
@@ -6,12 +6,11 @@
 */
 class TETRIS{
   constructor(){
-    this.arrTetris=[];  //10*20
+    this.arrTetris=[];  //20*10
 
     /*界面設置*/
     this.eleCanvas=document.querySelector('.canvas_main');
     this.eleCanvasNext=document.querySelector('.canvas_next');
-    this.ctx=this.eleCanvas.getContext('2d');
     this.W=10;  //寬度：10
     this.H=20;  //高度：20
     this.cell=30; //每個格子大小
@@ -19,7 +18,7 @@ class TETRIS{
     /*遊戲中的狀態*/
     this.score=0; //遊戲得分
     this.HiScore=0; //歷史最高分
-    this.next=['I',0];  //下一個方塊
+    this.next=['Z',1];  //下一個方塊
     this.level=18;  //18級
     this.lines=0; //已消除的行數
 
@@ -62,29 +61,6 @@ class TETRIS{
     ];  //消除行數得分
 
     //定義方塊形狀(4*4)(IJLOSTZ)(four times four)
-    /*this.f_f={
-      'I':[
-        [4,4,4,4],[0,15,0,0]
-      ],
-      'J':[
-        [4,4,6,0],[0,1,7,0],[6,2,2,0],[0,7,4,0]
-      ],
-      'L':[
-        [2,2,6,0],[0,7,1,0],[6,4,4,0],[0,4,7,0]
-      ],
-      'O':[
-        [0,6,6,0]
-      ],
-      'S':[
-        [0,6,3,0],[2,6,4,0]
-      ],
-      'T':[
-        [0,7,2,0],[4,6,4,0],[0,2,7,0],[2,6,2,0]
-      ],
-      'Z':[
-        [0,3,6,0],[4,6,2,0]
-      ]
-    };*/
     this.f_f={
       I:['4_4_4_4','0_15_0_0'],
       J:['4_4_6_0','0_1_7_0','6_2_2_0','0_7_4_0'],
@@ -92,8 +68,8 @@ class TETRIS{
       O:['0_6_6_0'],
       S:['0_6_3_0','2_6_4_0'],
       T:['0_7_2_0','4_6_4_0','0_2_7_0','2_6_2_0'],
-      Z:['0_3_6_0','4_6_2_0'],
-    }
+      Z:['0_3_6_0','4_6_2_0']
+    };
 
   }
 
@@ -103,6 +79,7 @@ class TETRIS{
     // this.raf(); //動畫：每秒60幀
   }
 
+  // 動畫
   raf(){
     var f=this;
     var iRAF=0;
@@ -124,16 +101,16 @@ class TETRIS{
   //初始化遊戲板數據
   initArrTetris(){
     var f=this;
-    for(var i=0;i<f.W;i++){
-      if(!this.arrTetris[i]){
-        this.arrTetris[i]=[];
+    for(var row=0;row<f.H;row++){
+      if(!this.arrTetris[row]){
+        this.arrTetris[row]=[];
       }
-      for(var j=0;j<f.H;j++){
-        if(j>=12){
+      for(var j=0;j<f.W;j++){
+        if(row>=12){
           //最後8行(13,20)
-          this.arrTetris[i][j]=Math.random()>.4?0:1;
+          this.arrTetris[row][j]=Math.random()>.4?0:1;
         }else{
-          this.arrTetris[i][j]=0;
+          this.arrTetris[row][j]=0;
         }
       }
     }
@@ -143,77 +120,35 @@ class TETRIS{
   //繪製canvas
   render(){
     var f=this;
-    f.renderCanvasMain();
-    f.renderCanvasNext();
-
+    f.renderCanvas(f.eleCanvas.getContext('2d'),f.arrTetris,f.cell);
+    f.renderCanvas(f.eleCanvasNext.getContext('2d'),f.F2(f.f_f[f.next[0]][f.next[1]]),22);
     return f;
   }
-  //畫主canvas
-  renderCanvasMain(){
+  //由二維數組繪製成canvas
+  renderCanvas(ctx,arr,perW){
     var f=this;
-    var ctx=f.ctx;
-    var w=f.cell;
     var arrBCC=[1,4];
-    // var arrBCC=[1,5,9];
-
-    ctx.clearRect(0,0,300,600);
-    
-
+    ctx.clearRect(0,0,1e4,1e4);
     ctx.beginPath();
-    for(var i=0;i<f.W;i++){
-      for(var j=0;j<f.H;j++){
-        if(f.arrTetris[i][j]){
-          ctx.fillStyle='black';
-          ctx.fillRect(w*i,w*j,w,w);
-
+    for(var row=0;row<arr.length;row++){
+      for(var j=0;j<arr[0].length;j++){
+        if(arr[row][j]){
           ctx.fillStyle='dimgray';
-          ctx.fillRect(w*i+arrBCC[0],w*j+arrBCC[0],w-2,w-2);
+          ctx.fillRect(perW*j+arrBCC[0],perW*row+arrBCC[0],perW-2,perW-2);
 
           //主要的顏色
           var arrColor=['#fffeff','#ca4679','#2eb788'];
           ctx.fillStyle=arrColor[0];
-          ctx.fillRect(w*i+arrBCC[1],w*j+arrBCC[1],w-2*arrBCC[1],w-2*arrBCC[1]);
-
-          // ctx.fillRect(w*i+arrBCC[2],w*j+arrBCC[2],w-2*arrBCC[2],w-2*arrBCC[2]);
+          ctx.fillRect(perW*j+arrBCC[1],perW*row+arrBCC[1],perW-2*arrBCC[1],perW-2*arrBCC[1]);
         }
       }
 
     }
     ctx.fill();
-
-    
-    
-    
-    
     return f;
   }
-  //畫下一個
-  renderCanvasNext(){
-    var f=this;
-    var next=f.f_f[f.next[0]][f.next[1]];
-    //'4_4_4_4'
-    var ctx=f.eleCanvasNext.getContext('2d');
-    var w=f.cell;
-    var arrBCC=[1,4];
-    ctx.clearRect(0,0,120,120);
-    ctx.beginPath();
-    var i=1;
 
-
-    
-
-    ctx.fillStyle='dimgray';
-    ctx.fillRect(w*i+arrBCC[0],w*i+arrBCC[0],w-2,w-2);
-
-    //主要的顏色
-    var arrColor=['#fffeff','#ca4679','#2eb788'];
-    ctx.fillStyle=arrColor[0];
-    ctx.fillRect(w*i+arrBCC[1],w*i+arrBCC[1],w-2*arrBCC[1],w-2*arrBCC[1]);
-f.F2('0_7_1_0');
-
-    return f;
-  }
-  //'4_4_4_4' ==> [[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]]
+  //將字母轉化為01二維數組 ('4_4_4_4' ==> [[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]])
   F2(LETTER){
     var arr=LETTER.split('_').map(function(v){
       var arr01=[];
@@ -233,9 +168,6 @@ f.F2('0_7_1_0');
       }
       return (arr01);
     });
-
-
-console.log(arr);
     return arr;
   }
 
@@ -243,5 +175,3 @@ console.log(arr);
 
 var obj=new TETRIS();
 obj.init();
-
-
