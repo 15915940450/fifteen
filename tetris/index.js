8102c2b9ecd45d2e64b3a086945e08f7dff96412
15915940450 <15915940450@139.com>
2019-07-09 14:07:24 +0800

    start AI

--- tetris/index.js
+++ tetris/index.js
@@ -17,7 +17,7 @@ class TETRIS{
     this.W=10;  //寬度：10
     this.H=20;  //高度：20
     this.cell=30; //每個格子大小
-    this.randomCell=3;  //隨機格子,false(0),1,2,3,,,18
+    this.randomRow=_.random(18);  //隨機格子,false(0),1,2,3,,,18
     this.lock=false; //鎖住遊戲
 
     /*遊戲中的狀態*/
@@ -124,7 +124,7 @@ class TETRIS{
     this.initArrTetris();
     this.initActive();
     this.initNext();
-    this.initPureRow();
+    this.initPureRow(); //乾淨的一行
     this.further();
     this.portrait();
     //加上當前活動方塊：arrTetrisAppendActive
@@ -138,6 +138,61 @@ class TETRIS{
     //右側文字:分數，level，行數
     this.htmlStatus();
 
+  }
+  //初始化遊戲板數據
+  initArrTetris(){
+    var f=this;
+    //初始雜亂數據
+    for(var row=0;row<f.H;row++){
+      if(!this.arrTetris[row]){
+        this.arrTetris[row]=[];
+      }
+      for(var j=0;j<f.W;j++){
+        if(row>=f.H-f.randomRow){
+          this.arrTetris[row][j]={
+            color:'crimson',
+            v:Math.random()>.4?0:1
+          };
+        }else{
+          this.arrTetris[row][j]={
+            color:'black',
+            v:0
+          };
+        }
+      }
+    }
+    return f;
+  }
+  initActive(){
+    var f=this;
+    //第一個當前活動的方塊
+    var active=f.gLETTER();
+    // f.active='I';
+    // f.activeForm=0;
+
+    f.active=active.LETTER;
+    f.activeForm=active.form;
+    
+    f.activePosition.row=f.calcAppearRow();
+    // f.activePosition.row=-2;  //0,1,2,3,4
+    return f;
+  }
+  initNext(){
+    var f=this;
+    var next=f.gLETTER();
+    f.next=next.LETTER;
+    f.nextForm=next.form;
+    return f;
+  }
+  initPureRow(){
+    var f=this;
+    for(var i=0;i<f.W;i++){
+      f.pureRow[i]={
+        color:'black',  //orchid
+        v:0
+      };
+    }
+    return f;
   }
   //改造數據(rule)
   further(){
@@ -162,196 +217,17 @@ class TETRIS{
 
     return f;
   }
-  //乾淨的一行
-  initPureRow(){
+  //判斷豎屏（肖像）（風景）
+  portrait(){
     var f=this;
-    for(var i=0;i<f.W;i++){
-      f.pureRow[i]={
-        color:'black',  //orchid
-        v:0
-      };
+    var CW=document.documentElement.clientWidth || document.body.clientWidth;
+    var CH=document.documentElement.clientHeight || document.body.clientHeight;
+    if(CW<CH){
+      document.querySelector('.canvas_main').style.width='59vw';
     }
     return f;
   }
-  initNext(){
-    var f=this;
-    var next=f.gLETTER();
-    f.next=next.LETTER;
-    f.nextForm=next.form;
-    return f;
-  }
-  initActive(){
-    var f=this;
-    //第一個當前活動的方塊
-    var active=f.gLETTER();
-    // f.active='I';
-    // f.activeForm=0;
-
-    f.active=active.LETTER;
-    f.activeForm=active.form;
-    
-    f.activePosition.row=f.calcAppearRow();
-    // f.activePosition.row=-2;  //0,1,2,3,4
-    return f;
-  }
-
-
-  // 動畫
-  raf(){
-    var f=this;
-    var iRAF=0;
-    // var t0=new Date().getTime();
-
-    var rafCallback=function(){
-      if(f.lock){
-        //鎖住，不增長
-        iRAF=0;
-      }else{
-        iRAF++;
-      }
-      // console.log(iRAF); //一直都在運行
-      // console.log(!iRAF%48); //0
-      var frame=f.rule.find(function(v){
-        return (v.level===f.level);
-      }).frame;
-      if(+(iRAF%frame)===1){
-        f.handleDown();
-      }
-      /*var t=new Date().getTime()-t0;  
-      if(t>1000 && t<2000){
-
-        console.log('x'); //每秒60幀
-      }*/
-      window.requestAnimationFrame(rafCallback);
-    };
-    window.requestAnimationFrame(rafCallback);
-    return f;
-  }
-
-  //初始化遊戲板數據
-  initArrTetris(){
-    var f=this;
-    //初始雜亂數據
-    for(var row=0;row<f.H;row++){
-      if(!this.arrTetris[row]){
-        this.arrTetris[row]=[];
-      }
-      for(var j=0;j<f.W;j++){
-        if(row>=f.H-f.randomCell){
-          this.arrTetris[row][j]={
-            color:'crimson',
-            v:Math.random()>.4?0:1
-          };
-        }else{
-          this.arrTetris[row][j]={
-            color:'black',
-            v:0
-          };
-        }
-      }
-    }
-    return f;
-  }
-  //計算當前活動方塊出現的行數值
-  calcAppearRow(){
-    var f=this;
-    var row=-3;
-    var activeLETTER=f.f_f[f.active].form[f.activeForm].split('_').reverse();
-    // console.log(activeLETTER);
-    for(var i=0;i<activeLETTER.length;i++){
-      if(!+activeLETTER[i]){
-        row++;
-      }else{
-        //跳出
-        break;
-      }
-    }
-    return (row);
-  }
-  // 計算當前活動方塊左右空值
-  calcLR(LETTER01){
-    var i;
-    var L=0;
-    var R=0;
-    var LETTER10=[];
-    for(i=0;i<LETTER01.length;i++){
-      LETTER10[i]=LETTER10[i] || [];
-      for(var j=0;j<LETTER01[0].length;j++){
-        LETTER10[i][j]=LETTER01[j][i];
-      }
-    }
-    var LETTER=LETTER10.map(function(v){
-      return (_.sumBy(v,function(v2){
-        return (v2.v);
-      }));
-    });
-    // console.log(LETTER);
-    for(i=0;i<LETTER.length;i++){
-      if(!LETTER[i]){
-        L++;
-      }else{
-        break;
-      }
-    }
-    for(i=LETTER.length;i>0;i--){
-      if(!LETTER[i-1]){
-        R++;
-      }else{
-        break;
-      }
-    }
-    // console.log(L);
-    // console.log(R);
-    return ({
-      L:L,
-      R:R
-    });
-  }
-
-
-  //渲染：繪製canvas
-  render(){
-    var f=this;
-    // 渲染主區
-    f.renderCanvas(f.eleCanvas.getContext('2d'),f.arrTetrisAppendActive,f.cell);
-    
-    // 渲染則區（下一個）
-    f.renderCanvas(f.eleCanvasNext.getContext('2d'),f.F2(f.next,f.nextForm),22);
-    return f;
-  }
-  //由二維數組繪製成canvas
-  renderCanvas(ctx,arr,perW){
-    var f=this;
-    var arrBCC=[1,4];
-    ctx.clearRect(0,0,1e4,1e4);
-    ctx.beginPath();
-    for(var row=0;row<arr.length;row++){
-      for(var j=0;j<arr[0].length;j++){
-        if(arr[row][j].v){
-          ctx.fillStyle='dimgray';
-          ctx.fillRect(perW*j+arrBCC[0],perW*row+arrBCC[0],perW-2,perW-2);
-
-          //格子的顏色
-          ctx.fillStyle=arr[row][j].color;
-          ctx.fillRect(perW*j+arrBCC[1],perW*row+arrBCC[1],perW-2*arrBCC[1],perW-2*arrBCC[1]);
-        }
-      }
-
-    }
-    ctx.fill();
-    return f;
-  }
-  //隨機生成字母
-  gLETTER(){
-    var f=this;
-    var LETTER=_.sample('IJLOSTZ'.split(''));
-    var form=_.random(f.f_f[LETTER].form.length-1);
-    return ({
-      LETTER:LETTER,
-      form:form
-    });
-  }
-  //數據添加當前活動方塊數據
+  //arrTetrisAppendActive=arrTetris+LETTER01;
   addActiveLETTER(){
     var f=this;
     var LETTER01=f.F2(f.active,f.activeForm);
@@ -383,42 +259,81 @@ class TETRIS{
 
     return f;
   }
-
-  /*
-  將字母轉化為01二維數組 ('4_4_4_4')
-  next:下一個字母，比如'J'
-  nextForm:下一個字母的形態序號，比如1
-  */
-  F2(next,nextForm){
-    var f=this;
-    var color=f.f_f[next].color;
-    var LETTER=f.f_f[next].form[nextForm];
-    var arr=LETTER.split('_').map(function(v){
-      var arr01=[];
-
-      // 15>>4(0) 15>>3(1) 15>>2(3) 15>>1(7) 15>>0(15)
-      // 2<<0(2) 2<<1(4) 2<<2(8) 2<<3(16)
-      // 4>>4(0) 4>>3(0) 4>>2(1) 4>>1(2) 4>>0(4)
-      for(var bit=3;bit>=0;bit--){
-        var objCell={
-          color:color,
-          v:0
-        };
-        if(v>>bit){
-          //該位有值
-          objCell={
-            color:color,
-            v:1
-          };
-          //減去該位餘值繼續檢測
-          v=v-(2<<(bit-1));
-        }
-        arr01.unshift(objCell);
-      }
-      return (arr01);
-    });
-    return arr;
+  // 確保arrTetris的索引
+  checkBorder(rowTetris,jTerris){
+    var b=(rowTetris>=0 && rowTetris<20 && jTerris>=0 && jTerris<10);
+    return b;
   }
+  
+  //渲染：繪製canvas
+  render(){
+    var f=this;
+    // 渲染主區
+    f.renderCanvas(f.eleCanvas,f.arrTetrisAppendActive);
+    
+    // 渲染則區（下一個）
+    f.renderCanvas(f.eleCanvasNext,f.F2(f.next,f.nextForm),22);
+    return f;
+  }
+  //由二維數組繪製成canvas
+  renderCanvas(el,arr,perW){
+    var f=this;
+    var arrBCC=[1,4];
+    var ctx=el.getContext('2d');
+    perW=perW || f.cell;
+    ctx.clearRect(0,0,1e4,1e4);
+    ctx.beginPath();
+    for(var row=0;row<arr.length;row++){
+      for(var j=0;j<arr[0].length;j++){
+        if(arr[row][j].v){
+          ctx.fillStyle='dimgray';
+          ctx.fillRect(perW*j+arrBCC[0],perW*row+arrBCC[0],perW-2,perW-2);
+
+          //格子的顏色
+          ctx.fillStyle=arr[row][j].color;
+          ctx.fillRect(perW*j+arrBCC[1],perW*row+arrBCC[1],perW-2*arrBCC[1],perW-2*arrBCC[1]);
+        }
+      }
+
+    }
+    ctx.fill();
+    return f;
+  }
+  
+
+
+  // 動畫(handleDown)
+  raf(){
+    var f=this;
+    var iRAF=0;
+    // var t0=new Date().getTime();
+
+    var rafCallback=function(){
+      if(f.lock){
+        //鎖住，不增長
+        iRAF=0;
+      }else{
+        iRAF++;
+      }
+      // console.log(iRAF); //一直都在運行
+      // console.log(!iRAF%48); //0
+      var frame=f.rule.find(function(v){
+        return (v.level===f.level);
+      }).frame;
+      if(+(iRAF%frame)===1){
+        f.handleDown();
+      }
+      /*var t=new Date().getTime()-t0;  
+      if(t>1000 && t<2000){
+
+        console.log('x'); //每秒60幀
+      }*/
+      window.requestAnimationFrame(rafCallback);
+    };
+    window.requestAnimationFrame(rafCallback);
+    return f;
+  }
+
 
   //監聽鍵盤
   listen(){
@@ -604,11 +519,7 @@ class TETRIS{
 
     return true;
   }
-  // 確保arrTetris的索引
-  checkBorder(rowTetris,jTerris){
-    var b=(rowTetris>=0 && rowTetris<20 && jTerris>=0 && jTerris<10);
-    return b;
-  }
+  
   //下一回合
   round(){
     var f=this;
@@ -654,6 +565,10 @@ class TETRIS{
 
     return f;
   }
+
+
+
+
   //檢查級數
   inspectLevel(){
     var f=this;
@@ -688,18 +603,18 @@ class TETRIS{
     }
 
     //立馬消失
-    f.renderCanvas(f.eleCanvas.getContext('2d'),arrAnimate,f.cell);
+    f.renderCanvas(f.eleCanvas,arrAnimate);
     // 出現
     window.setTimeout(function(){
-      f.renderCanvas(f.eleCanvas.getContext('2d'),arrAnimate0,f.cell);
+      f.renderCanvas(f.eleCanvas,arrAnimate0);
     },100);
     // 消失
     window.setTimeout(function(){
-      f.renderCanvas(f.eleCanvas.getContext('2d'),arrAnimate,f.cell);
+      f.renderCanvas(f.eleCanvas,arrAnimate);
     },300);
     // 出現
     window.setTimeout(function(){
-      f.renderCanvas(f.eleCanvas.getContext('2d'),arrAnimate0,f.cell);
+      f.renderCanvas(f.eleCanvas,arrAnimate0);
       //放開鎖定
       f.lock=false;
     },500);
@@ -741,16 +656,111 @@ class TETRIS{
     f.elsStatus[2].innerHTML=this.lines;
     return f;
   }
-  //判斷豎屏（肖像）（風景）
-  portrait(){
+
+  
+  //計算當前活動方塊出現的行數值
+  calcAppearRow(){
     var f=this;
-    var CW=document.documentElement.clientWidth || document.body.clientWidth;
-    var CH=document.documentElement.clientHeight || document.body.clientHeight;
-    if(CW<CH){
-      document.querySelector('.canvas_main').style.width='59vw';
+    var row=-3;
+    var activeLETTER=f.f_f[f.active].form[f.activeForm].split('_').reverse();
+    // console.log(activeLETTER);
+    for(var i=0;i<activeLETTER.length;i++){
+      if(!+activeLETTER[i]){
+        row++;
+      }else{
+        //跳出
+        break;
+      }
     }
-    return f;
+    return (row);
   }
+  // 計算當前活動方塊左右空值
+  calcLR(LETTER01){
+    var i;
+    var L=0;
+    var R=0;
+    var LETTER10=[];
+    for(i=0;i<LETTER01.length;i++){
+      LETTER10[i]=LETTER10[i] || [];
+      for(var j=0;j<LETTER01[0].length;j++){
+        LETTER10[i][j]=LETTER01[j][i];
+      }
+    }
+    var LETTER=LETTER10.map(function(v){
+      return (_.sumBy(v,function(v2){
+        return (v2.v);
+      }));
+    });
+    // console.log(LETTER);
+    for(i=0;i<LETTER.length;i++){
+      if(!LETTER[i]){
+        L++;
+      }else{
+        break;
+      }
+    }
+    for(i=LETTER.length;i>0;i--){
+      if(!LETTER[i-1]){
+        R++;
+      }else{
+        break;
+      }
+    }
+    // console.log(L);
+    // console.log(R);
+    return ({
+      L:L,
+      R:R
+    });
+  }
+  //隨機生成字母
+  gLETTER(){
+    var f=this;
+    var LETTER=_.sample('IJLOSTZ'.split(''));
+    var form=_.random(f.f_f[LETTER].form.length-1);
+    return ({
+      LETTER:LETTER,
+      form:form
+    });
+  }
+  
+
+  /*
+  將字母轉化為01二維數組 ('4_4_4_4')
+  next:下一個字母，比如'J'
+  nextForm:下一個字母的形態序號，比如1
+  */
+  F2(next,nextForm){
+    var f=this;
+    var color=f.f_f[next].color;
+    var LETTER=f.f_f[next].form[nextForm];
+    var arr=LETTER.split('_').map(function(v){
+      var arr01=[];
+
+      // 15>>4(0) 15>>3(1) 15>>2(3) 15>>1(7) 15>>0(15)
+      // 2<<0(2) 2<<1(4) 2<<2(8) 2<<3(16)
+      // 4>>4(0) 4>>3(0) 4>>2(1) 4>>1(2) 4>>0(4)
+      for(var bit=3;bit>=0;bit--){
+        var objCell={
+          color:color,
+          v:0
+        };
+        if(v>>bit){
+          //該位有值
+          objCell={
+            color:color,
+            v:1
+          };
+          //減去該位餘值繼續檢測
+          v=v-(2<<(bit-1));
+        }
+        arr01.unshift(objCell);
+      }
+      return (arr01);
+    });
+    return arr;
+  } //method F2
+  
 
 
 
