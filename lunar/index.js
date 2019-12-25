66a1e74bf310ff2947affed2c1ecdc02fba5570f
15915940450 <15915940450@139.com>
2019-12-25 13:48:03 +0800

    add lunar

---
+++ lunar/index.js
@@ -0,0 +1,58 @@
+/*
+https://baike.baidu.com/item/农历/67925
+*/
+class Basic{
+  constructor(){
+    this.n=-1;  //raf多少次
+    this.interval=10; //每幀的間隔
+    this.currentStep=-1; //當前。。。
+
+    this.CW=document.documentElement.clientWidth || document.body.clientWidth;
+    this.CH4=document.documentElement.clientHeight || document.body.clientHeight;
+  }
+
+  //判断动画是否 持续 进行
+  ciZuk(){
+    return (this.n<1e1*this.interval);
+  }
+
+  init(){
+    
+  }
+
+  // 算法
+  solve(){
+    var f=this;
+    f.raf();
+    return f;
+  }
+  //執行動畫
+  raf(){
+    var f=this;
+    var rafCallback=function(){
+      f.n++;
+      //動畫进行中
+      if(f.ciZuk()){
+        if(!(f.n%f.interval)){
+          //一帧一步
+          f.currentStep++;
+          f.doINeveryframe();
+        }
+        window.requestAnimationFrame(rafCallback);
+      } //end if
+    };
+    window.requestAnimationFrame(rafCallback);
+    return f;
+  } //raf
+  //每一幀你要做點什麽？
+  doINeveryframe(){
+    var f=this;
+    console.log(f.currentStep);
+    return f;
+  }
+
+} //class
+
+var obj=new Basic();
+obj.init();
+obj.solve();
